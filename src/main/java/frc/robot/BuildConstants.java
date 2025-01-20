9c21f540a6c2b95b2500f2b4b5221cc4135fea6f
Node-Kid <weiszhaarwill@gmail.com>
2025-01-20 17:17:17 -0500

    misc changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "ef51e46658f8c1e58e3e80e2aadd5b7ebd149006";
-  public static final String GIT_DATE = "2025-01-18 15:16:44 EST";
+  public static final int GIT_REVISION = 36;
+  public static final String GIT_SHA = "456a7f53af35b117332d42a8b68995ed69db6183";
+  public static final String GIT_DATE = "2025-01-20 16:51:17 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-19 21:37:49 EST";
-  public static final long BUILD_UNIX_TIME = 1737340669024L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-20 17:16:24 EST";
+  public static final long BUILD_UNIX_TIME = 1737411384738L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
