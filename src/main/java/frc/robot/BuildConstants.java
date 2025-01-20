3b5c64c09744f4e34da794486481491b93bfa081
Mason Hayes <masonthayes@gmail.com>
2025-01-20 16:52:43 -0500

    na

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "9ba299e7430d7cd8c19bfe53e0a759f4397cf11f";
-  public static final String GIT_DATE = "2025-01-18 11:26:42 EST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "8addf0ab7fa494d2ecc497153a3d0501988c988a";
+  public static final String GIT_DATE = "2025-01-18 14:52:16 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-18 14:49:36 EST";
-  public static final long BUILD_UNIX_TIME = 1737229776810L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-18 15:33:15 EST";
+  public static final long BUILD_UNIX_TIME = 1737232395497L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
