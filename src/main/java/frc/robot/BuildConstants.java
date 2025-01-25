a21181bc9737bc7cb17da1824c5f640b669afed0
Low-Debug <Landon.stromain@gmail.com>
2025-01-25 10:07:31 -0500

    updated slapdown Algae

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "3b5c64c09744f4e34da794486481491b93bfa081";
-  public static final String GIT_DATE = "2025-01-20 16:52:43 EST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "1bab979ee9f069e9dde70b619e0f803de52e9d4f";
+  public static final String GIT_DATE = "2025-01-20 18:36:05 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-20 18:13:08 EST";
-  public static final long BUILD_UNIX_TIME = 1737414788764L;
+  public static final String BUILD_DATE = "2025-01-25 09:57:10 EST";
+  public static final long BUILD_UNIX_TIME = 1737817030714L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
