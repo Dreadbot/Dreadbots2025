9969ce3f2f53bc47bb6ffb5431473a3cbf3412a6
Node-Kid <weiszhaarwill@gmail.com>
2025-01-25 14:09:06 -0500

    more choreo paths for camera angle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "112d4f6080a4530e172e0392c390562b7734e648";
-  public static final String GIT_DATE = "2025-01-18 00:42:09 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "c638c047a8f2751528bd9a6fba2790c88d3ce271";
+  public static final String GIT_DATE = "2025-01-20 16:33:34 EST";
   public static final String GIT_BRANCH = "auto-align";
-  public static final String BUILD_DATE = "2025-01-19 23:22:55 EST";
-  public static final long BUILD_UNIX_TIME = 1737346975130L;
+  public static final String BUILD_DATE = "2025-01-25 12:17:39 EST";
+  public static final long BUILD_UNIX_TIME = 1737825459106L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
