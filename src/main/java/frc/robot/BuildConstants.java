8ac05a965da627bef49fc177b4700c35f1ed3fde
Will Weiszhaar <weiszhaarwill@gmail.com>
2025-01-28 17:36:44 -0500

    example

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "c638c047a8f2751528bd9a6fba2790c88d3ce271";
-  public static final String GIT_DATE = "2025-01-20 16:33:34 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "d7f15987c13d2975572e9805b36f473b2a45cf48";
+  public static final String GIT_DATE = "2025-01-28 17:33:21 EST";
   public static final String GIT_BRANCH = "auto-align";
-  public static final String BUILD_DATE = "2025-01-25 12:17:39 EST";
-  public static final long BUILD_UNIX_TIME = 1737825459106L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-28 17:33:52 EST";
+  public static final long BUILD_UNIX_TIME = 1738103632637L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
