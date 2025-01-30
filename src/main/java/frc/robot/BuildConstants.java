16eb26b764c0a09bfbad90b04a586d00d0a56269
Node-Kid <weiszhaarwill@gmail.com>
2025-01-30 12:24:12 -0500

    modified gitignore to revmoe build constants from pushing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 49;
-  public static final String GIT_SHA = "aa6b5ac4663b1acded6a0bece930449c84eb51c1";
-  public static final String GIT_DATE = "2025-01-25 12:04:31 EST";
+  public static final int GIT_REVISION = 57;
+  public static final String GIT_SHA = "6000c41d2ff8be148760f32b6aafa2026de0f401";
+  public static final String GIT_DATE = "2025-01-28 13:07:15 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-28 12:25:58 EST";
-  public static final long BUILD_UNIX_TIME = 1738085158506L;
+  public static final String BUILD_DATE = "2025-01-28 13:12:40 EST";
+  public static final long BUILD_UNIX_TIME = 1738087960354L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
