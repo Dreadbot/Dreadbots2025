05b96ec1252505d3aa27383759ea9cf0ecceb1ed
Node-Kid <weiszhaarwill@gmail.com>
2025-02-03 13:08:02 -0500

    added other pois

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "61ad83e4b47c83eb35cbef2d9268d4e40b405e6c";
-  public static final String GIT_DATE = "2025-01-25 14:27:00 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "60eeae1855b374e0577144a499546c02bcf8924f";
+  public static final String GIT_DATE = "2025-01-30 13:15:22 EST";
   public static final String GIT_BRANCH = "auto-align";
-  public static final String BUILD_DATE = "2025-01-30 13:06:20 EST";
-  public static final long BUILD_UNIX_TIME = 1738260380618L;
+  public static final String BUILD_DATE = "2025-02-03 13:06:38 EST";
+  public static final long BUILD_UNIX_TIME = 1738605998968L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/util/AutoAlignUtil.java
+++ src/main/java/frc/robot/util/AutoAlignUtil.java
@@ -1,7 +1,6 @@
 package frc.robot.util;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -19,30 +18,53 @@ public class AutoAlignUtil {
 
 	private static double fieldSizeX = Units.feetToMeters(57.573);
 	private static double fieldSizeY = Units.feetToMeters(26.417);
-	public static List<Pose2d> POIs;
+	public static ArrayList<Pose2d> POIs = new ArrayList<Pose2d>();
 	private static Alert createdPOIAlert = new Alert("Generated POI List!", AlertType.kInfo);
 	/**
-	 * This function generates all the POIs for the Autoalign command, requires that we know which side of the field we are on, so needs to called at runtime
+	 * This function generates all the POIs for the Auto align command, requires that we know which side of the field we are on, so needs to called at runtime
 	 */
 	public static void buildPOIList() {
-		Pose2d[] tmp = new Pose2d[6];
+		Pose2d[] reefFaces = new Pose2d[6];
 		// Reef Auto Align Poses
 		Translation2d reefCenter = new Translation2d(Units.inchesToMeters(176.746), Units.inchesToMeters(158.501));
 		Translation2d firstSide = new Translation2d(3.229, 4.026);
 		for(int i = 0; i < 6; i++) {
 			Translation2d side = firstSide.rotateAround(reefCenter, Rotation2d.fromDegrees(60 * (i - 1)));
-			tmp[i] = getAlliancePOI(
+			reefFaces[i] = getAlliancePOI(
 				new Pose2d(side, Rotation2d.fromDegrees(60 * (i - 1)))
 			);
+			POIs.add(reefFaces[i]);
 		}
+		// Station Pick up Poses, get alliance specifc position then mirror, less code
+		Pose2d leftFarPickup = getAlliancePOI(
+			new Pose2d(1.645, 7.504, Rotation2d.fromRadians(2.186))
+		);
+		Pose2d leftClosePickup = getAlliancePOI(
+			new Pose2d(0.584, 6.705, Rotation2d.fromRadians(2.186))
+		);
 
-		POIs = List.of(tmp);
+		Pose2d rightFarPickup = mirrorPose(leftFarPickup);
+		Pose2d rightClosePickup = mirrorPose(leftClosePickup);
+		// Processor
+
+		Pose2d processor = getAlliancePOI(
+			new Pose2d(5.970, 0.510, Rotation2d.fromRadians(Math.PI / 2))
+		);
+
+		//add poses to arraylist;
+		POIs.add(leftFarPickup);
+		POIs.add(leftClosePickup);
+		POIs.add(rightClosePickup);
+		POIs.add(rightFarPickup);
+		POIs.add(processor);
+		Pose2d[] loggerPoses = POIs.toArray(new Pose2d[0]);
+		Logger.recordOutput("AutoAlign/GeneratedPOIs", Pose2d.struct, loggerPoses);
 	}
 
 	/**
 	 * Takes a POI and flips it to the other side of the field if alliance is red
 	 * @param poi The POI to flip
-	 * @return The Alliance specific POI
+	 * @return The alliance specific POI
 	 */
 	public static Pose2d getAlliancePOI(Pose2d poi) {
 
@@ -53,6 +75,17 @@ public class AutoAlignUtil {
 		}
 
 		return poi;
+	}
+
+	/**
+	 *  Flips the pose over the Y axis for certain positions we want to align to
+	 * @param pose
+	 * @return the mirrored psoe
+	 */
+	private static Pose2d mirrorPose(Pose2d pose) {
+		Translation2d mirroredTranslation = new Translation2d(pose.getX(), fieldSizeY - pose.getY());
+		Rotation2d mirroredRotation = pose.getRotation().unaryMinus(); // Geometry stuff I think 
+		return new Pose2d(mirroredTranslation, mirroredRotation);
 	}
 
 	public static Command createPOIListCommand() {
