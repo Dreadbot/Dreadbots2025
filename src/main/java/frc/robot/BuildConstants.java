4b32afc69b78f5f1c9ffdf3579f70a0a8c46e0f7
Arman Buyukbozkirli <arman12cb@gmail.com>
2025-02-04 18:12:10 -0500

    secondary controller

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 58;
-  public static final String GIT_SHA = "16eb26b764c0a09bfbad90b04a586d00d0a56269";
-  public static final String GIT_DATE = "2025-01-30 12:24:12 EST";
+  public static final int GIT_REVISION = 59;
+  public static final String GIT_SHA = "2eb462ef80a051bb9bc75255fcb4b179253254bf";
+  public static final String GIT_DATE = "2025-02-04 17:57:57 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-04 17:57:06 EST";
-  public static final long BUILD_UNIX_TIME = 1738709826061L;
+  public static final String BUILD_DATE = "2025-02-04 18:09:28 EST";
+  public static final long BUILD_UNIX_TIME = 1738710568974L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
