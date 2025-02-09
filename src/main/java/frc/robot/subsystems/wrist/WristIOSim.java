437220c5c3443b4848c401f84a83d8122f066b8c
Trig_ <weiszhaarwill@gmail.com>
2025-02-08 21:12:19 -0500

    misc changes

--- src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
@@ -2,10 +2,7 @@ package frc.robot.subsystems.wrist;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-import frc.robot.Constants.WristConstants;
 
 
 public class WristIOSim implements WristIO {
