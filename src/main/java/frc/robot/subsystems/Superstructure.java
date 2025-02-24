ada3e5a5cfc6bcc3ece5967a654c28e5067dac35
Node-Kid <weiszhaarwill@gmail.com>
2025-02-24 16:56:10 -0500

    changed requestSuperstructureState to be safe

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -6,6 +6,7 @@ import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.WristConstants;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.wrist.Wrist;
 
@@ -20,7 +21,6 @@ public class Superstructure {
 
     public Command requestSuperstructureState(SuperstructureState level) {
         //If we are in dangerZone, run angle first, and then go to position
-
         /*
          * Commands.either(
          * OnTrue,
@@ -29,17 +29,18 @@ public class Superstructure {
          * )
          */
         return Commands.either(
-            wrist.setAngleDegrees(level.angle)
+            wrist.setAngleDegrees(SuperstructureState.STOW.angle)
                 .andThen(Commands.waitUntil(wrist::atSetpoint))
-                .andThen(elevator.riseTo(level.height)),
-
+                .andThen(elevator.riseTo(level.height))
+                .andThen(Commands.waitUntil(elevator::atHeight))
+                .andThen(wrist.setAngleDegrees(level.angle)),
             elevator.riseTo(level.height)
                 .andThen(wrist.setAngleDegrees(level.angle)),
 
-            () -> wrist.isInDangerZone() && (level == SuperstructureState.L4)
+            () -> wrist.isInDangerZone() || (level == SuperstructureState.PICKUP) // Pickup requires a wrist angle that is inside danger zone
         );
     }
-    //Height, Angle (degrees)
+    //Height (meters), Angle (degrees)
     public static enum SuperstructureState {
         L1(0.634, 110.0),
         L2(0.996, 65.6),
