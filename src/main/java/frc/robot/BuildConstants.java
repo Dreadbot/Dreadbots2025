260ed293bcf9c54854f8d5be72d47c23b6329b7f
Will Weiszhaar <weiszhaarwill@gmail.com>
2025-02-28 08:35:37 -0500

    Milford pre-qual tuning, elevator, slapdown

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 157;
-  public static final String GIT_SHA = "ad0f976d1ec223ff9f189f750eee307c4de3c4b7";
-  public static final String GIT_DATE = "2025-02-25 21:03:16 EST";
+  public static final int GIT_REVISION = 158;
+  public static final String GIT_SHA = "e5c740c875378cd806b22375f9e71f17e409387d";
+  public static final String GIT_DATE = "2025-02-26 21:06:10 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-02-26 21:03:33 EST";
-  public static final long BUILD_UNIX_TIME = 1740621813021L;
+  public static final String BUILD_DATE = "2025-02-28 08:33:09 EST";
+  public static final long BUILD_UNIX_TIME = 1740749589212L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
