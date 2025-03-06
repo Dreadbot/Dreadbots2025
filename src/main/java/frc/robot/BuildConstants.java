66536dee690f4d97b60153f3fa14b376f1852568
Odog0 <c3pooliver@gmail.com>
2025-03-06 13:02:25 -0500

    knockout algae fr this time

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 163;
-  public static final String GIT_SHA = "747be942b711d2fdd584cdc2575938884791a7d8";
-  public static final String GIT_DATE = "2025-03-03 18:11:55 EST";
+  public static final int GIT_REVISION = 164;
+  public static final String GIT_SHA = "f7332c3b15a3e7eaa58d526248b06a1f7aa7cca4";
+  public static final String GIT_DATE = "2025-03-03 18:36:31 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-03-03 18:28:19 EST";
-  public static final long BUILD_UNIX_TIME = 1741044499186L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-04 16:29:30 EST";
+  public static final long BUILD_UNIX_TIME = 1741123770444L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
