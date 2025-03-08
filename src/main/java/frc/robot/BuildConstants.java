98dede7d3951376a6c3d302c30973eafd5905b33
Will Weiszhaar <weiszhaarwill@gmail.com>
2025-03-08 16:55:18 -0500

    general work on 2 coral

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 181;
-  public static final String GIT_SHA = "036c17d554286559f9fbfc8613a07bdd14b9b5cb";
-  public static final String GIT_DATE = "2025-03-08 13:13:18 EST";
+  public static final int GIT_REVISION = 182;
+  public static final String GIT_SHA = "5e6692d8d148a647cee7c9068eb7e2edb982dd40";
+  public static final String GIT_DATE = "2025-03-08 16:27:18 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-03-08 16:26:09 EST";
-  public static final long BUILD_UNIX_TIME = 1741469169085L;
+  public static final String BUILD_DATE = "2025-03-08 16:50:01 EST";
+  public static final long BUILD_UNIX_TIME = 1741470601811L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
