14c28d6e0b5f21dc81eddffb0919476fc2e63cc9
FyNnNiL <fynn.soren.nielsen@gmail.com>
2025-03-10 19:03:23 -0400

    added checking whether vision pose makes sense *not tested

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 182;
-  public static final String GIT_SHA = "566a79f330b1ecb146cdebd3dcae41fd3f9d3d32";
-  public static final String GIT_DATE = "2025-03-08 13:37:41 EST";
-  public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-03-10 16:51:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1741639919126L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 185;
+  public static final String GIT_SHA = "13bdb732c70b40b606955a730766b3ecd937ad40";
+  public static final String GIT_DATE = "2025-03-10 17:15:11 EDT";
+  public static final String GIT_BRANCH = "ambiguity-test";
+  public static final String BUILD_DATE = "2025-03-10 17:24:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1741641855443L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
