2b68d38297811c63d99c00e5f75d896379e7fa7d
unknown <weiszhaarwill@gmail.com>
2025-04-02 18:47:30 -0400

    Middle D barge auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 221;
-  public static final String GIT_SHA = "2066237db4ba55ac1e521ad3f92fdf04834c9346";
-  public static final String GIT_DATE = "2025-03-29 14:14:54 EDT";
+  public static final int GIT_REVISION = 222;
+  public static final String GIT_SHA = "d30daa5070def01599e8134344b3899de3f9fa43";
+  public static final String GIT_DATE = "2025-04-01 20:17:02 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-04-01 20:14:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1743552879703L;
+  public static final String BUILD_DATE = "2025-04-02 18:45:04 EDT";
+  public static final long BUILD_UNIX_TIME = 1743633904885L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
