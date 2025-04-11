7e85995af4b7e3979917f40af6472e611c44b6d7
unknown <weiszhaarwill@gmail.com>
2025-04-10 21:02:03 -0400

    work on teleop

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 236;
-  public static final String GIT_SHA = "d5b5fbf542cb0aa34852b1935a23c9af92679fa9";
-  public static final String GIT_DATE = "2025-04-10 18:26:40 EDT";
+  public static final int GIT_REVISION = 237;
+  public static final String GIT_SHA = "4cee720832688c644738f34623852582e6c558c2";
+  public static final String GIT_DATE = "2025-04-10 20:07:34 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-04-10 19:57:35 EDT";
-  public static final long BUILD_UNIX_TIME = 1744329455040L;
+  public static final String BUILD_DATE = "2025-04-10 20:59:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1744333183385L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
